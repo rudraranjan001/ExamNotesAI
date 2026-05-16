0cc3c26bcdff47054239be3f69cc42ed5f10a79c
Rudra <125184056+rudraranjan001@users.noreply.github.com>
2026-05-16 14:27:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,6 +26,10 @@ app.use(cors(
     {origin:"https://examnotesaiclient-hw2m.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    },
+    {origin:"https://https://exam-notes-ai-steel.vercel.app",
+        credentials:true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
 ))
 
